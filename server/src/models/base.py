231058479d296c5345698b73df7831a9a6bec881
Jonnyishman <18678825+jonnyishman@users.noreply.github.com>
2025-09-11 20:23:32 +0100

    add declarative mapping to models (#48)

--- server/src/models/base.py
+++ server/src/models/base.py
@@ -1,8 +1,6 @@
 """SQLAlchemy database instance and base model."""
 from __future__ import annotations
 
-import datetime as dt
-
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
@@ -15,18 +13,3 @@ class Base(DeclarativeBase):
 
 # Create database instance
 db = SQLAlchemy(model_class=Base)
-
-
-class BaseModel(db.Model):
-    """Base model with common fields and methods."""
-
-    __abstract__ = True
-
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=dt.datetime.now(dt.UTC), nullable=False)
-    updated_at = db.Column(
-        db.DateTime,
-        default=dt.datetime.now(dt.UTC),
-        onupdate=dt.datetime.now(dt.UTC),
-        nullable=False
-    )
